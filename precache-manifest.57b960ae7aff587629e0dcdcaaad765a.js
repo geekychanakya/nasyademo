be7ff06f5bcc26e0be865cf997d188e31e757e5a
geekyChanakya <your-email@bangthetable.com>
2020-05-16 18:27:53 +0530

    Updates

---
+++ precache-manifest.57b960ae7aff587629e0dcdcaaad765a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bd89d1fd08b4bdc20f77e99c6c1e3ad",
+    "url": "/nasyademo/index.html"
+  },
+  {
+    "revision": "38c685182f3d9d9d0179",
+    "url": "/nasyademo/static/css/main.931230b8.chunk.css"
+  },
+  {
+    "revision": "fbcd03cf3b04725e864e",
+    "url": "/nasyademo/static/js/2.73190439.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/nasyademo/static/js/2.73190439.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38c685182f3d9d9d0179",
+    "url": "/nasyademo/static/js/main.3d095676.chunk.js"
+  },
+  {
+    "revision": "6f4a4139773051e3d036",
+    "url": "/nasyademo/static/js/runtime-main.59293424.js"
+  },
+  {
+    "revision": "cabb7f2295c0c9694ed3fb1db04f2f46",
+    "url": "/nasyademo/static/media/Logo.cabb7f22.png"
+  },
+  {
+    "revision": "f7c7ca0f1e15ff79398ea2a268cfa52a",
+    "url": "/nasyademo/static/media/landing-page1.f7c7ca0f.png"
+  },
+  {
+    "revision": "eec592d13e5462d502f17b06fb074a7f",
+    "url": "/nasyademo/static/media/landing-page2.eec592d1.png"
+  }
+]);
